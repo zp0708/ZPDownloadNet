e14def17d4cad44496ca23aaddcf2560fdcc5b6e
NewUnsigned <zhaopeng0708@icloud.com>
2015-04-29 00:05:47 +0800

    初始化项目

---
+++ ZPDownloadNet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZPDownloadNet
+//
+//  Created by 赵鹏 on 15/4/29.
+//  Copyright (c) 2015年 赵鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
